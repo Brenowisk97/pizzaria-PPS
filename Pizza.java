17f4e4e89b8ff5e82a75542933db6e317a544c24
Brenowisk97 <105371282+Brenowisk97@users.noreply.github.com>
2024-06-08 15:19:01 -0300

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,45 @@
+package pattern.domain;
+
+import java.util.ArrayList;
+
+Abstract public class Pizza{
+  public static enum Sabor{
+    Calabresa,
+    Pepperoni,
+    Queijo,
+    Vegana
+  }
+
+  String nome;
+  String massa;
+  String molho;
+  List<String> coberturas = new ArrayList<String>();
+
+  public String getNome(){
+    return nome;
+  }
+  public void preparar(){
+    System.out.println("Preparando " + nome);
+  }
+    public void assar(){
+    System.out.println("Assando " + nome);
+  }
+   public void cortar(){
+    System.out.println("Cortando " + nome);
+  }
+   public void embalar(){
+    System.out.println("Embalando " + nome);
+  }
+  public String toString(){
+    StringBuffer detalhes = new StringBuffer();
+      detalhes.append("***************************************\n");
+      detalhes.append("Pizza solicitada saiu para entrega!\n");
+      detalhes.append("***************************************\n");
+      detalhes.append("Classe:"+ this.getClass()getCanonicalName() + \this.getClass();
+      detalhes.append(nome+ "\n");
+      detalhes.append(massa+ "\n");
+      detalhes.append(molho+ "\n");
+      for (String cobertura : coberturas){
+          detalhes.append(cobertura+ "\n");
+      }
+    return detalhes.toString():
