405e15d431830847b716a4824c2eb85f21594a64
Brenowisk97 <105371282+Brenowisk97@users.noreply.github.com>
2024-06-08 15:52:06 -0300

    Create PizzaDeCalabresaCentro.java

---
+++ PizzaDeCalabresaCentro.java
@@ -0,0 +1,12 @@
+Package pattern.domain;
+
+public class PizzaDeCalabresaCentro extends Pizza{
+
+  public PizzaDeCalabresaCentro(){
+    nome = " Pizza de Calabresa";
+   massa = "Crosta Fina";
+   molho = "Molho de Alho Branco";
+  coberturas.add("Calabresa");
+  coberturas.add("Queijo Parmesão Ralado");
+  }
+}
