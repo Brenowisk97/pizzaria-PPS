1142e41dedda8733542f302b241e595b7ff43c8a
Brenowisk97 <105371282+Brenowisk97@users.noreply.github.com>
2024-06-08 17:16:40 -0300

    Create PizzaVegana.java

---
+++ PizzaVegana.java
@@ -0,0 +1,12 @@
+Package pattern.domain;
+
+public class PizzaVegana extends Pizza{
+
+  public PizzaVegana(){
+    nome = " Pizza Vegana";
+   massa = "Massa Integral";
+   molho = "Molho Queijo Vegano";
+  coberturas.add("Atum fresco");
+  coberturas.add("Brócolis");
+  }
+}
